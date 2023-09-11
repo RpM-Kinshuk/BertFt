0cd75dbfab83cb04e05a323556b7b4a4cc494b5c
Kinshuk Goel <103813028+RpM-Kinshuk@users.noreply.github.com>
2023-09-12 03:59:40 +0530

    Improved script for BERT training

--- bert.sh
+++ bert.sh
@@ -2,6 +2,7 @@ num_layers="0 1 2 3 4 5 6 8 10 12 18 24 30 36 72 74"
 task_list="cola mnli mrpc qnli qqp rte sst2 stsb wnli"
 alpha_list="True False"
 laynorm="False"
+batch_size=32
 
 for task in $task_list
 do
@@ -9,14 +10,14 @@ do
     do
         for layers in $num_layers
         do
-            save_path="/rscratch/tpang/kinshuk/RpMKin/bert_ft/task_$task/lay_norm_$laynorm/alpha_asc_$alpha/layers_$num_layers/lr2e-5_epoch3_bs32/"
-            python bertft.py \
+            save_path="/rscratch/tpang/kinshuk/RpMKin/bert_ft/task_$task/lay_norm_$laynorm/alpha_asc_$alpha/layers_$num_layers/lr2e-5_epoch3_bs$batch_size/"
+            OMP_NUM_THREADS=1 CUDA_VISIBLE_DEVICES=0 python bertft.py \
                 --savepath "$save_path" \
                 --epochs 3 \
                 --model_name bert-base-uncased \
                 --task_name "$task" \
                 --max_length 128 \
-                --batch_size 32 \
+                --batch_size $batch_size \
                 --learning_rate "2e-5" \
                 --seed 7 \
                 --freeze_bert True \
