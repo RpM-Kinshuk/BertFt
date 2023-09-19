28c0b570f24f4c41eb12ebf198ffba399dc80d64
Kinshuk Goel <103813028+RpM-Kinshuk@users.noreply.github.com>
2023-09-20 03:24:00 +0530

    Updated gputracker

--- shells/run_exp.py
+++ shells/run_exp.py
@@ -1,52 +1,55 @@
 import itertools
-from gputrek.gputrek import get_logger, DispatchThread
+from gputracker.gputracker import get_logger, DispatchThread
 
 gpus = list(range(8))
 train_layers = [0, 1, 2, 3, 4, 5, 6, 8, 10, 12, 18, 24, 30, 36, 72, 74, 80]
 task_list = ['cola', 'mnli', 'mrpc', 'qnli', 'qqp', 'rte', 'sst2', 'stsb', 'wnli']
 
 model = "bert-base-uncased"
-ascending_order = ["False", "True"]
-add_layernorm = ["False", "True"]
-freeze_bert = ["False", "True"]
-train_seed_lst = [3, 5, 7]
-seed = train_seed_lst[1]
-max_length = 512
+ascending_order = ["True", "False"]
 
+norm = "False"
+freeze_bert = "True"
+train_seed_lst = [3, 5, 7]
+seed = 7
+max_length = 128
 batch_size = 32
-epochs = 20
+epochs = 3
+
+task = "cola"
 
 logger = get_logger('log', 'schedule_subspace.log')
 
 grid = list(
     itertools.product(
-        add_layernorm, ascending_order, train_layers, task_list)
+        ascending_order, train_layers)
 )
 BASH_COMMAND_LIST = []
 
-for norm, order, layers, task in grid:
+for order, layers in grid:
     
     save_path = "/rscratch/tpang/kinshuk/RpMKin/bert_ft" + \
-        f"/lay_norm_{norm}/alpha_asc_{order}/layers_{layers}/task_{task}/lr2e-5_epoch{epochs}_bs{batch_size}/"
+        f"/task_{task}/lay_norm_{norm}/alpha_asc_{order}/layers_{layers}/lr2e-5_epoch3_bs{batch_size}/"
 
     cmd = "OMP_NUM_THREADS=1 python bertft.py " + \
         f"--savepath {save_path} " + \
-        f"--seed {seed} " + \
         f"--epochs {epochs} " + \
         f"--model_name {model} " + \
-        f"--add_layer_norm {norm} " + \
-        f"--freeze_bert {freeze_bert[0]} " + \
-        f"--alpha_ascending {order} " + \
-        f"--num_layers {layers} " + \
         f"--task_name {task} " + \
-        f"--batch_size {batch_size} " + \
         f"--max_length {max_length} " + \
-        f"--slow_tokenizer True" + \
-        f"--pad_to_max_length False " + \
+        f"--batch_size {batch_size} " + \
+        f"--learning_rate 2e-5 " + \
+        f"--seed {seed} " + \
+        f"--freeze_bert {freeze_bert} " + \
+        f"--num_layers {layers} " + \
+        f"--alpha_ascending {order} " + \
+        f"--slow_tokenizer True " + \
+        f"--pad_to_max_length True " + \
+        f"--add_layer_norm {norm} " + \
         f"--max_train_steps 1000 " + \
         f"--grad_acc_steps 1 " + \
-        f"--accelerate True " + \
-        f"--learning_rate 2e-5 "
+        f"--accelerate False " + \
+        f"--debug False"
         
     BASH_COMMAND_LIST.append(cmd)
 
